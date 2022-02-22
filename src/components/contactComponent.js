dcf3c9e4589f45b3b1d98eff85a9695b7be86238
VirenderRana <virender.rana0009@gmail.com>
2022-02-21 19:56:07 -0600

    fixed contact

--- src/components/contactComponent.js
+++ src/components/contactComponent.js
@@ -2,11 +2,6 @@ import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Button, Label, Col, Row } from 'reactstrap';
 import {Link} from 'react-router-dom';
 
-const required = (val) => val && val.length;
-const maxLength = (len) => (val) => !(val) || (val.length <= len);
-const minLength = (len) => (val) => (val) && (val.length >= len);
-const isNumber = (val) => !isNaN(Number(val));
-const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]\.[A-Z]{2,4}$/i.test(val);
 
 const Contact = () => {
   function handleSubmit(values) {
