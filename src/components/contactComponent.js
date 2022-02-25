fc605f0a1bd46d79576ab248495b541687324a0f
Morgan Jackson <morjack@siue.edu>
2022-02-24 23:47:37 -0600

    fixed michael's email one more place

--- src/components/contactComponent.js
+++ src/components/contactComponent.js
@@ -50,7 +50,7 @@ const Contact = () => {
                     <div className="col-12 col-sm-11 offset-sm-1">
                         <div className="btn-group" role="group">
                             {/**<a role="button" className="btn btn-primary" href="tel:+85212345678"><i className="fa fa-phone"></i> Call</a>**/}
-                            <a role="button" className="btn btn-success" href="mailto:mfrench@siue.edu"><i className="fa fa-envelope-o"></i> Email</a>
+                            <a role="button" className="btn btn-success" href="mailto:mifrenc@siue.edu"><i className="fa fa-envelope-o"></i> Email</a>
                         </div>
                     </div>
                 </div>
